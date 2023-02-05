d5de888ab78c50b864654d83c97bf8258da71061
Sofy Korzhova <sofykorzhova@gmail.com>
2023-02-05 16:34:02 +0100

    Append tests

--- tests/Validator/CurrentUserValidatorTest.php
+++ tests/Validator/CurrentUserValidatorTest.php
@@ -3,11 +3,12 @@
 namespace Sofyco\Bundle\SecurityValidationBundle\Tests\Validator;
 
 use PHPUnit\Framework\MockObject\MockObject;
-use Sofyco\Bundle\SecurityValidationBundle\Tests\App\Message\UpdateUser;
 use Sofyco\Bundle\SecurityValidationBundle\Validator\CurrentUser;
 use Sofyco\Bundle\SecurityValidationBundle\Validator\CurrentUserValidator;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\User\InMemoryUser;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 use Symfony\Component\Validator\Test\ConstraintValidatorTestCase;
 
 final class CurrentUserValidatorTest extends ConstraintValidatorTestCase
@@ -19,6 +20,13 @@ final class CurrentUserValidatorTest extends ConstraintValidatorTestCase
         $this->security = $this->createMock(Security::class);
 
         parent::setUp();
+    }
+
+    public function testInvalidConstraint(): void
+    {
+        $this->expectException(UnexpectedTypeException::class);
+
+        $this->validator->validate('0000-1111-2222-3333', new NotBlank());
     }
 
     public function testValidationSkippedByNonLoggedUser(): void
@@ -49,6 +57,20 @@ final class CurrentUserValidatorTest extends ConstraintValidatorTestCase
         $this->security->method('getUser')->willReturn($user);
 
         $this->validator->validate('0000-1111-2222-3333', new CurrentUser());
+
+        $this->assertNoViolation();
+    }
+
+    public function testValidationPassedByAllowedRoles(): void
+    {
+        $user = new InMemoryUser(username: '0000-1111-2222-3334', password: null, roles: ['ROLE_ADMIN']);
+        $constraint = new CurrentUser();
+        $constraint->roles = ['ROLE_ADMIN'];
+
+        $this->security->method('getUser')->willReturn($user);
+        $this->security->method('isGranted')->with('ROLE_ADMIN')->willReturn(true);
+
+        $this->validator->validate('0000-1111-2222-3333', $constraint);
 
         $this->assertNoViolation();
     }
